e9dfc6de132801b2e1afc5e837450b7dc1a576e7
moyodancer <yost@calpoly.edu>
2016-02-08 23:20:31 -0800

    minor clean ups

--- HW1/gaussOD.m
+++ HW1/gaussOD.m
@@ -1,4 +1,4 @@
-function [ r, v, RSite, qHat, q, tau1, tau3 ] = gaussOD(alpha, delta, siteInfo)
+function [ rKeep, vKeep, RSiteKeep, qHatKeep, qKeep, tau1Keep, tau3Keep ] = gaussOD(alpha, delta, siteInfo)
 %gaussOD Gauss method for orbit determination for an oject whose RAAN and 
 %Decclination were observed at 3 different time steps within 10 degrees of 
 %each other. ASSUMPTIONS: Coplanar observations (no pertibations between 
@@ -24,37 +24,50 @@ global NUM_OBSV
 mu = 398600;
 r = 0; v = 0;
 % Get RSite vectors
-RSite = getRSite(siteInfo);
-%RSite = [4054.881 3956.224 3905.073; 2748.195 2888.232 2956.935; 4074.237 4074.364 4074.430]
-% Get slant range unit vector
-qHat = getQHat(alpha, delta); 
-%qHat = [.9473 .5742 .3007; .0155 .5747 .7399; .3201 .5830 .6018];
-% get r2
-tau = getTau(siteInfo, 3, 1);
-tau3 = getTau(siteInfo, 3, 2);
-tau1 = getTau(siteInfo, 1, 2);
-
- r2 = sym('r2');
-[a1, a1u] = getAs(tau, tau3);
-[a3, a3u] = getAs(tau, tau1);
-M = inv(qHat)*RSite;
-d1 = M(2, 1)*a1 - M(2, 2) +M(2, 3)*a3;
-d2 = M(2, 1)*a1u +M(2,3)*a3u;
-c = dot(qHat(:, 2), RSite(:, 2));
-rSite2 = norm(RSite(:, 2));
-eqn = r2^8-(d1^2 +2*c*d1+rSite2^2)*r2^6-2*mu*(c*d2+d1*d2)*r2^3-mu^2*d2^2;
-ans = double(solve(eqn==0, r2));
-realR2 = ans(find(ans(find(imag(ans)==0))>0,1));
-
-%Use r2 to get r1 and r3 using lagrange multipliers
-[f1, g1] = getLagrange(realR2, tau1);
-[f3, g3] = getLagrange(realR2, tau3);
-u = mu/(realR2^3);
-c1 = a1+a1u*u;
-c2 = -1;
-c3 = a3+a3u*u;
-q = inv(qHat)*RSite*[-c1; -c2; -c3]./[c1; c2; c3];
-r = RSite + qHat*diag(q);
-v = 1/(f1*g3-f3*g1)*(-f3*r(:,1)+f1*r(:,3));
+for i = 1:3:NUM_OBSV
+    clear r2 eqn d1 d2 c rSite2
+    RSite = getRSite(siteInfo, i);
+    RSite = RSite(:, i:i+2);
+    %RSite = [4054.881 3956.224 3905.073; 2748.195 2888.232 2956.935; 4074.237 4074.364 4074.430]
+    % Get slant range unit vector
+    qHat = getQHat(alpha(i:i+2), delta(i:i+2));
+    %qHat = [.9473 .5742 .3007; .0155 .5747 .7399; .3201 .5830 .6018];
+    % get r2
+    tau = getTau(siteInfo, i+2, i);
+    tau3 = getTau(siteInfo, i+2, i+1);
+    tau1 = getTau(siteInfo, i, i+1);
+    
+    r2 = sym('r2');
+    [a1, a1u] = getAs(tau, tau3);
+    [a3, a3u] = getAs(tau, tau1);
+    M = inv(qHat)*RSite;
+    d1 = M(2, 1)*a1 - M(2, 2) +M(2, 3)*a3;
+    d2 = M(2, 1)*a1u +M(2,3)*a3u;
+    c = dot(qHat(:, 2), RSite(:, 2));
+    rSite2 = norm(RSite(:, 2));
+    eqn = r2^8-(d1^2 +2*c*d1+rSite2^2)*r2^6-2*mu*(c*d2+d1*d2)*r2^3-mu^2*d2^2;
+    ans = double(solve(eqn==0, r2));
+    realR2 = ans(find(ans(find(imag(ans)==0))>0,1));
+    
+    %Use r2 to get r1 and r3 using lagrange multipliers
+    [f1, g1] = getLagrange(realR2, tau1);
+    [f3, g3] = getLagrange(realR2, tau3);
+    u = mu/(realR2^3);
+    c1 = a1+a1u*u;
+    c2 = -1;
+    c3 = a3+a3u*u;
+    q = inv(qHat)*RSite*[-c1; -c2; -c3]./[c1; c2; c3];
+    r = RSite + qHat*diag(q);
+    v = 1/(f1*g3-f3*g1)*(-f3*r(:,1)+f1*r(:,3));
+    
+    % Store for returns
+    RSiteKeep(:, i:i+2) = RSite;
+    qHatKeep(:, i:i+2) = qHat;
+    qKeep(:, i) = q;
+    rKeep(:, i:i+2) = r;
+    vKeep(:, i) = v;
+    tau1Keep(i) = tau1;
+    tau3Keep(i) = tau3;
+end
 end
 
--- HW1/getQHat.m
+++ HW1/getQHat.m
@@ -4,8 +4,7 @@ function [ qHat ] = getQHat( alpha, delta )
 %   @param delta    DEC of each observation 1x3, degrees
 %   @return qHat    Unit vector of slant range for each of the three
 %                   observations, one column vector per observation, 3x3
-global NUM_OBSV
-for i = 1:NUM_OBSV
+for i = 1:3
     qHat(1, i) = cosd(delta(i))*cosd(alpha(i));
     qHat(2, i) = cosd(delta(i))*sind(alpha(i));
     qHat(3, i) = sind(delta(i));
